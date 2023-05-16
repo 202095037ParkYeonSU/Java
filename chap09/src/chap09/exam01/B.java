1a0c3f5d50ce124563434d294a5e50770d4a04fb
202095037ParkYeonSU <129822447+202095037ParkYeonSU@users.noreply.github.com>
2023-05-16 18:36:51 +0900

    Add files via upload

---
+++ chap09/src/chap09/exam01/B.java
@@ -0,0 +1,7 @@
+package chap09.exam01;
+
+public class B {
+	A a; // 가능(A클래스 접근 가능)
+	
+
+}
---
+++ chap09_1/src/chap09_1/EarthTest.java
@@ -0,0 +1,11 @@
+package chap09_1;
+
+public class EarthTest {
+
+	public static void main(String[] args) {
+		System.out.println("Earth area1 : " + Earth.EARTH_RADIUS + "km");
+		System.out.println("Earth area2 : " + Earth.EARTH_AREA + "km^2");
+		
+	}
+
+}
